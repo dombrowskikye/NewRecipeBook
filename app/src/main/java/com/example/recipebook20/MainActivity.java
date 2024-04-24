ee9bce4edf28da196c2598e73828ff681b47b682
Dennon03 <dennon.parks@ucdenver.edu>
2024-04-23 20:27:48 -0600

    added an actual recipe

--- app/src/main/java/com/example/recipebook20/MainActivity.java
+++ app/src/main/java/com/example/recipebook20/MainActivity.java
@@ -38,7 +38,22 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnR
 
         originalList.add(new Recipe("Tacos","These are the Instructions","These are the Ingredients","Lunch"));
 
-        list.add(new Recipe("Tacos","These are the Instructions","These are the Ingredients","Lunch"));
+        list.add(new Recipe("Tacos","Add ground beef and a chopped onion to a skillet over medium high heat. Once beef is cooked through drain the grease grease. Add all seasonings and beef broth.\n" +
+                "Bring to a boil then lower the heat. Cover and let simmer for about 20 minutes until the liquid has reduced.\n" +
+                "In a separate pan, heat the skillet over medium heat and add some oil to the skillet. Lay out a few of the tortillas and tip them with shredded Monterey Jack cheese. Top each one with a spoonful of taco meat. Fold the tortillas over and cook on both sides until golden brown and crispy.\n" +
+                "Lay the tacos out on paper towels to absorb any extra grease. Top tacos with your favorite toppings and enjoy!","2 lb ground beef\n" +
+                "1 chopped onion\n" +
+                "2 TBSP minced garlic\n" +
+                "1 tsp each of salt and pepper\n" +
+                "1/2 tsp cayenne\n" +
+                "4 tsp chili powder\n" +
+                "2 tsp each of cumin and dried cilantro\n" +
+                "1 1/2 cups of beef broth\n" +
+                "White corn tortillas\n" +
+                "1 tbsp oil\n" +
+                "Shredded Monterey Jack cheese\n" +
+                "Taco Toppings sour cream,\n" +
+                "Pico de Gallo, taco sauce, shredded lettuce","Lunch"));
         recipeAdapter = new RecipeAdapter(this, list,this);
 
 
